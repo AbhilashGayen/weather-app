c1150533ffbe2fdb0d0106156e653928300cdd94
Abhilash Gayen <abhilashgayen@Abhilashs-MacBook-Pro.local>
2020-05-28 00:08:20 +0530

    Updates

---
+++ precache-manifest.e6d20156cc00a7316e181db867f2005b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a199441bfb41f93370396eabe9b5d8f",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "53ac34cdff0b9ca78527",
+    "url": "/weather-app/static/css/2.98c86ea1.chunk.css"
+  },
+  {
+    "revision": "467d97deb8e014b5a5ca",
+    "url": "/weather-app/static/css/main.114552bd.chunk.css"
+  },
+  {
+    "revision": "53ac34cdff0b9ca78527",
+    "url": "/weather-app/static/js/2.39444083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.39444083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "467d97deb8e014b5a5ca",
+    "url": "/weather-app/static/js/main.8e47fb73.chunk.js"
+  },
+  {
+    "revision": "55676f7ecad5f9b6942e",
+    "url": "/weather-app/static/js/runtime-main.58bc7688.js"
+  },
+  {
+    "revision": "b4298796593d851ec54659a34b5920af",
+    "url": "/weather-app/static/media/abstract-art-background-color-2249527.b4298796.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
